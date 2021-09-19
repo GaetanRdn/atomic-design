7f3859b5748b802366ea53c1729ad3b19e620709
Gaetan Redin <gaetan.redin@cbp-group.com>
2021-09-19 18:02:36 +0200

    feature(coercion): Create coerce-boolean-input decorator and apply it inside button directive

--- src/components/atoms/button/button.directive.spec.ts
+++ src/components/atoms/button/button.directive.spec.ts
@@ -3,62 +3,81 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ButtonDirective } from './button.directive';
 
 describe('ButtonDirective', () => {
-  let fixture: ComponentFixture<HostComponent>;
+  describe('Main use cases', () => {
+    let fixture: ComponentFixture<HostComponent>;
 
-  beforeEach(() => {
-    fixture = TestBed.configureTestingModule({
-      declarations: [ButtonDirective, HostComponent]
-    }).createComponent(HostComponent);
+    beforeEach(() => {
+      fixture = TestBed.configureTestingModule({
+        declarations: [ButtonDirective, HostComponent]
+      }).createComponent(HostComponent);
+    });
+
+    test('should create default button', async () => {
+      // WHEN
+      fixture.detectChanges();
+      await fixture.whenRenderingDone();
+
+      // THEN
+      expect(fixture.debugElement.children[0].nativeElement).toMatchSnapshot();
+    });
+
+    test.each([
+      'small',
+      'medium',
+      'large'
+    ] as ('small' | 'medium' | 'large')[])('when size is %s then check snapshot', (size: 'small' | 'medium' | 'large') => {
+      // GIVEN
+      fixture.componentInstance.size = size;
+
+      // WHEN
+      fixture.detectChanges();
+
+      // THEN
+      expect(fixture.debugElement.children[0].nativeElement).toMatchSnapshot();
+    });
+
+    test.each([true, false])('when outlined is set to %s then check snapshot', (outlined: boolean) => {
+      // GIVEN
+      fixture.componentInstance.outlined = outlined;
+
+      // WHEN
+      fixture.detectChanges();
+
+      // THEN
+      expect(fixture.debugElement.children[0].nativeElement).toMatchSnapshot();
+    });
+
+    test.each([
+      'primary',
+      'accent',
+      'warn'
+    ] as ('primary' | 'accent' | 'warn')[])('when color is %s then check snapshot', (color: 'primary' | 'accent' | 'warn') => {
+      // GIVEN
+      fixture.componentInstance.color = color;
+
+      // WHEN
+      fixture.detectChanges();
+
+      // THEN
+      expect(fixture.debugElement.children[0].nativeElement).toMatchSnapshot();
+    });
   });
 
-  test('should create default button', async () => {
-    // WHEN
-    fixture.detectChanges();
-    await fixture.whenRenderingDone();
+  describe('Coerce outlined case', () => {
+    let fixture: ComponentFixture<CoerceOutlinedComponent>;
 
-    // THEN
-    expect(fixture.debugElement.children[0].nativeElement).toMatchSnapshot();
-  });
+    beforeEach(() => {
+      fixture = TestBed.configureTestingModule({
+        declarations: [ButtonDirective, CoerceOutlinedComponent]
+      }).createComponent(CoerceOutlinedComponent);
 
-  test.each([
-    'small',
-    'medium',
-    'large'
-  ] as ('small' | 'medium' | 'large')[])('when size is %s then check snapshot', (size: 'small' | 'medium' | 'large') => {
-    // GIVEN
-    fixture.componentInstance.size = size;
+      fixture.detectChanges();
+    });
 
-    // WHEN
-    fixture.detectChanges();
-
-    // THEN
-    expect(fixture.debugElement.children[0].nativeElement).toMatchSnapshot();
-  });
-
-  test.each([true, false])('when outlined is set to %s then check snapshot', (outlined: boolean) => {
-    // GIVEN
-    fixture.componentInstance.outlined = outlined;
-
-    // WHEN
-    fixture.detectChanges();
-
-    // THEN
-    expect(fixture.debugElement.children[0].nativeElement).toMatchSnapshot();
-  });
-
-  test.each([
-    'primary',
-    'accent',
-    'warn'
-  ] as ('primary' | 'accent' | 'warn')[])('when color is %s then check snapshot', (color: 'primary' | 'accent' | 'warn') => {
-    // GIVEN
-    fixture.componentInstance.color = color;
-
-    // WHEN
-    fixture.detectChanges();
-
-    // THEN
-    expect(fixture.debugElement.children[0].nativeElement).toMatchSnapshot();
+    test('should generate outlined button', () => {
+      expect(fixture.debugElement.children[0].nativeElement).toMatchSnapshot();
+      expect(fixture.debugElement.children[0].classes['adr-outlined']).toBe(true);
+    });
   });
 });
 
@@ -72,4 +91,11 @@ class HostComponent {
   public outlined?: boolean;
 
   public color?: 'primary' | 'accent' | 'warn';
+}
+
+@Component({
+  template: `
+      <button adrButton outlined>Click Me</button>`
+})
+class CoerceOutlinedComponent {
 }
