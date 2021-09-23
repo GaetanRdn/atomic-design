6551a571cf25940a557a95f54765f5c62a7ed1eb
Gaetan Redin <gaetan.redin@cbp-group.com>
2021-09-23 22:19:26 +0200

    feature(ButtonDirective): Add story
    Closes #14

---
+++ src/components/atoms/button/button.stories.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Meta, Story } from '@storybook/angular';
+import { ButtonDirective } from 'src/components/atoms/button/button.directive';
+
+@Component({
+  selector: 'adr-dummy',
+  template: `
+      <button adrButton [size]="size" [outlined]="outlined" [color]="color">Click</button>`
+})
+class DummyComponent {
+  @Input()
+  public size: 'small' | 'medium' | 'large' = 'medium';
+
+  @Input()
+  public outlined: boolean = false;
+
+  @Input()
+  public color?: 'primary' | 'accent' | 'warn';
+}
+
+export default {
+  title: 'atoms/button',
+  component: DummyComponent
+} as Meta;
+
+const Template: Story<DummyComponent> = (args) => ({
+  props: args, moduleMetadata: { declarations: [ButtonDirective], imports: [CommonModule] }
+});
+
+export const Default = Template.bind({});
+Default.args = {};
+
