8ef0b7aededfa16c4b348e78ad8e1c245567aa97
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2021-10-02 16:16:57 +0200

    Feat input directive (#28)
    * feature(atoms/input): Create inputdirective
    
    Closes #16
    
    * feature(decorator): Create autounsubscribe decorator for subscriptable objects
    
    * refactor(input): Use AutoUnsubscribe decorator
    
    * feature(input): Create basic story
    
    * style(input): Add style to input
    
    default, disabled, readonly, hover & focus
    
    * refactor(input): set valueChange readonly
    
    * feature(InputDirective): Make it work with ReactiveForms
    
    * feature(InputDirective): Add story for reactive form
    
    * feature(InputDirective): Remove story for reactive form
    
    * feature(InputDirective): Add touched on focus
    
    * feature(InputDirective): Handle disabled from control
    
    * review(InputDirective): clean snapshot and module
    
    Co-authored-by: Gaetan Redin <gaetan.redin@cbp-group.com>

---
+++ src/components/atoms/forms/input/input.directive.ts
@@ -0,0 +1,89 @@
+import { Directive, EventEmitter, HostBinding, HostListener, Input, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { AutoUnsubscribe } from '../../../core/common/auto-unsubscribe.decorator';
+import { CoerceBoolean } from '../../../core/common/coerce-boolean-inputs.decorator';
+
+@Directive({
+  selector: 'input[adrInput]',
+  host: {
+    class: 'adr-input',
+    '[class.adr-focused]': 'focused || null',
+    '[class.adr-readonly]': 'readonly || null',
+    '[class.adr-disabled]': 'disabled || null'
+  },
+  providers: [{ provide: NG_VALUE_ACCESSOR, useExisting: InputDirective, multi: true }]
+})
+@AutoUnsubscribe()
+export class InputDirective implements ControlValueAccessor {
+  @HostBinding('attr.value')
+  @Input()
+  public value: any;
+
+  @Input()
+  @HostBinding('readonly')
+  @CoerceBoolean()
+  public readonly: boolean = false;
+
+  @Output()
+  public readonly valueChange: EventEmitter<any> = new EventEmitter<any>();
+
+  @CoerceBoolean()
+  private _disabled: boolean = false;
+
+  get disabled(): boolean {
+    return this._disabled;
+  }
+
+  @Input()
+  @HostBinding('disabled')
+  set disabled(disabled) {
+    this._disabled = disabled;
+  }
+
+  private _focused: boolean = false;
+
+  get focused(): boolean {
+    return this._focused;
+  }
+
+  public writeValue(value: any): void {
+    this.value = value;
+  }
+
+  public registerOnChange(fn: any): void {
+    this._onChange = fn;
+  }
+
+  public registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+
+  public setDisabledState(disabled: boolean): void {
+    this.disabled = disabled;
+  }
+
+  @HostListener('input', ['$event.target.value'])
+  public onInput(value: any): void {
+    if (!this.readonly && !this.disabled) {
+      this.value = value;
+      this.valueChange.emit(value);
+      this._onChange(value);
+      this._onTouched();
+    }
+  }
+
+  @HostListener('focus', ['true'])
+  @HostListener('blur', ['false'])
+  public onToggleFocus(focused: boolean): void {
+    if (!this.readonly && !this.disabled) {
+      this._focused = focused;
+      this._onTouched();
+    }
+  }
+
+  protected _onChange = (_: any): void => {
+  };
+
+  protected _onTouched = (): void => {
+  };
+}
---
+++ src/components/atoms/forms/input/input.stories.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { action } from '@storybook/addon-actions';
+import { Meta, Story } from '@storybook/angular';
+import { InputDirective } from 'src/components/atoms/forms/input/input.directive';
+
+@Component({
+  selector: 'adr-input-dummy',
+  template: `
+      <input adrInput [disabled]="disabled" [readonly]="readonly" [value]="value" (valueChange)="valueChange.emit($event)"
+             placeholder="default input" />`
+})
+class InputDummyComponent {
+  @Input()
+  public value: any;
+
+  @Input()
+  public readonly: boolean = false;
+
+  @Input()
+  public disabled: boolean = false;
+
+  @Output()
+  public valueChange: EventEmitter<any> = new EventEmitter<any>();
+}
+
+export default {
+  title: 'atoms/forms/input',
+  argTypes: {
+    value: { control: { type: 'text' } }
+  },
+  component: InputDummyComponent
+} as Meta;
+
+const BasicTemplate: Story<InputDummyComponent> = (args) => ({
+  props: {
+    ...args,
+    valueChange: action('log')
+  },
+  moduleMetadata: { declarations: [InputDirective] }
+});
+
+export const Default = BasicTemplate.bind({});
