073ba7801230d219da66a79bfe04292aeb048b85
Gaetan Redin <gaetan.redin@cbp-group.com>
2021-10-02 14:24:15 +0200

    feature(InputDirective): Add story for reactive form

--- src/components/atoms/forms/input/input.stories.ts
+++ src/components/atoms/forms/input/input.stories.ts
@@ -1,15 +1,16 @@
-import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { action } from '@storybook/addon-actions';
 import { Meta, Story } from '@storybook/angular';
+import { Observable } from 'rxjs';
 import { InputDirective } from 'src/components/atoms/forms/input/input.directive';
 
 @Component({
-  selector: 'adr-dummy',
+  selector: 'adr-input-dummy',
   template: `
       <input adrInput [disabled]="disabled" [readonly]="readonly" [value]="value" (valueChange)="valueChange.emit($event)" />`
 })
-class InputComponent {
+class InputDummyComponent {
   @Input()
   public value: any;
 
@@ -25,15 +26,56 @@ class InputComponent {
 
 export default {
   title: 'atoms/forms/input',
-  component: InputComponent,
   argTypes: {
     value: { control: { type: 'text' } }
   }
 } as Meta;
 
-const Template: Story<InputComponent> = (args) => ({
-  props: { ...args, valueChange: action('log') }, moduleMetadata: { declarations: [InputDirective], imports: [CommonModule] }
+const BasicTemplate: Story<InputDummyComponent> = (args) => ({
+  props: {
+    ...args,
+    valueChange: action('log')
+  },
+  moduleMetadata: { declarations: [InputDirective] },
+  component: InputDummyComponent
 });
 
-export const Default = Template.bind({});
+export const Default = BasicTemplate.bind({});
 
+/** Reactive forms **/
+
+@Component({
+  selector: 'adr-input-form-dummy',
+  template: `
+      <input adrInput [readonly]="readonly" [formControl]="formControl" />`
+})
+class InputReactiveFormDummyComponent {
+  @Input()
+  public readonly: boolean = false;
+
+  public formControl: FormControl = new FormControl(this['value']);
+
+  @Output()
+  public formValueChange: Observable<any> = this.formControl.valueChanges;
+
+  @Input()
+  set value(value: any) {
+    this.formControl.setValue(value);
+  }
+
+  @Input()
+  set disabled(disabled: boolean) {
+    disabled ? this.formControl.disable() : this.formControl.enable();
+  }
+}
+
+const ReactiveTemplate: Story<InputReactiveFormDummyComponent> = (args) => ({
+  props: {
+    ...args,
+    formValueChange: action('log')
+  },
+  moduleMetadata: { declarations: [InputDirective], imports: [ReactiveFormsModule] },
+  component: InputReactiveFormDummyComponent
+});
+
+export const ReactiveForm = ReactiveTemplate.bind({});
