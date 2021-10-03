e61878f4076d761025f2b5ed6a0542f495959886
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2021-10-03 14:03:35 +0200

    37 label component (#41)
    * chore(package.json): Add moduleNameMapper to fix paths into test files
    
    * feature(LabelComponent): Create it
    
    Closes #37
    
    * fix(LabelComponent): Fix content projection
    
    * style(LabelComponent): Add styles
    
    * feature(LabelComponent): Create story
    
    Co-authored-by: Gaetan Redin <gaetan.redin@cbp-group.com>

---
+++ src/components/atoms/forms/label/label.component.spec.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { TemplateLookup } from 'src/components/core/tests/template-lookup';
+import { LabelComponent } from './label.component';
+
+describe('LabelComponent', () => {
+  let templateLookup: TemplateLookup<HostComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LabelComponent, HostComponent]
+    });
+
+    templateLookup = new TemplateLookup<HostComponent>(TestBed.createComponent(HostComponent));
+  });
+
+  test.each(['toto', 'test'])('should create with content %s', (label: string) => {
+    // GIVEN
+    templateLookup.hostComponent.label = label;
+
+    // WHEN
+    templateLookup.detectChanges();
+
+    // THEN
+    expect(templateLookup.firstChildElement).toMatchSnapshot();
+  });
+});
+
+@Component({
+  template: `
+      <adr-label>{{label}}</adr-label>`
+})
+class HostComponent {
+  public label!: string;
+}
---
+++ src/components/atoms/forms/label/label.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "adr-label",
+  template: `<ng-content></ng-content>`,
+})
+export class LabelComponent {}
