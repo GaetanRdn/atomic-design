f49cd6c923fc2fe92ee39ef57d08c6c36c59c74f
Gaetan Redin <gaetan.redin@cbp-group.com>
2021-10-05 18:43:21 +0200

    refactor(Label stories): clean code

--- src/components/atoms/forms/label/label.stories.ts
+++ src/components/atoms/forms/label/label.stories.ts
@@ -1,15 +1,12 @@
-import { componentWrapperDecorator, Meta, Story } from "@storybook/angular";
-import { InputDirective } from "src/components/atoms/forms/input/input.directive";
+import { Meta, Story } from "@storybook/angular";
 import { LabelComponent } from "src/components/atoms/forms/label/label.component";
 
 export default {
   title: "atoms/forms/label",
   component: LabelComponent,
-  decorators: [componentWrapperDecorator(LabelComponent)],
 } as Meta;
 
 const BasicTemplate: Story<LabelComponent> = (args) => ({
-  moduleMetadata: { declarations: [InputDirective] },
   template: `<adr-label>{{ ngContent }}</adr-label>`,
   props: { ...args },
 });
