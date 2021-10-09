ad7494d378befdc0607ad39232c7473949c2ce11
Gaetan Redin <gaetan.redin@cbp-group.com>
2021-10-09 16:03:00 +0200

    chore(templateLookup): add method to get component instance

--- src/components/core/tests/template-lookup.ts
+++ src/components/core/tests/template-lookup.ts
@@ -1,6 +1,6 @@
-import { DebugElement, Predicate, Type } from '@angular/core';
-import { ComponentFixture } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { DebugElement, Predicate, Type } from "@angular/core";
+import { ComponentFixture } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 
 export class TemplateLookup<T> {
   get firstChildElement(): HTMLElement {
@@ -11,8 +11,7 @@ export class TemplateLookup<T> {
     return this.fixture.componentInstance;
   }
 
-  constructor(public readonly fixture: ComponentFixture<T>) {
-  }
+  constructor(public readonly fixture: ComponentFixture<T>) {}
 
   public detectChanges(): void {
     this.fixture.detectChanges();
@@ -21,13 +20,17 @@ export class TemplateLookup<T> {
   public get(selectorOrType: string | Type<any>): DebugElement {
     let predicate: Predicate<DebugElement>;
 
-    if (typeof selectorOrType === 'string') {
+    if (typeof selectorOrType === "string") {
       predicate = By.css(selectorOrType);
     } else {
       predicate = By.directive(selectorOrType);
     }
 
     return this.fixture.debugElement.query(predicate);
+  }
+
+  public getComponent<U>(selectorOrType: string | Type<U>): U {
+    return this.get(selectorOrType).componentInstance;
   }
 
   public query<T extends HTMLElement>(selectorOrType: string | Type<any>): T {
