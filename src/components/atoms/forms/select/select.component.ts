cb86ae2f0c671c23587a7ea85349a441c86d0552
Gaetan Redin <gaetan.redin@cbp-group.com>
2021-10-10 16:00:52 +0200

    feature(Select): Init
    Closes #45

---
+++ src/components/atoms/forms/select/select.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'adr-select',
+    templateUrl: './select.component.html',
+    styleUrls: ['select.component.scss'],
+})
+export class SelectComponent {}
---
+++ src/components/atoms/forms/select/select.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SelectComponent } from './select.component';
+
+@NgModule({
+    declarations: [SelectComponent],
+    imports: [CommonModule],
+})
+export class SelectModule {}
