4668d5a0c5648dfd249a0610e4c89a05d9f0e8df
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2021-10-10 16:05:23 +0200

    fix storybook (#47)
    Co-authored-by: Gaetan Redin <gaetan.redin@cbp-group.com>

--- src/components/atoms/forms/autocomplete/autocomplete.stories.ts
+++ src/components/atoms/forms/autocomplete/autocomplete.stories.ts
@@ -1,25 +1,23 @@
-import { FormControl, ReactiveFormsModule } from "@angular/forms";
-import { action } from "@storybook/addon-actions";
-import { Meta, Story } from "@storybook/angular";
-import { AutocompleteComponent } from "./autocomplete.component";
-import { AutocompleteModule } from "./autocomplete.module";
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { action } from '@storybook/addon-actions';
+import { Meta, Story } from '@storybook/angular';
+import { AutocompleteComponent } from './autocomplete.component';
+import { AutocompleteModule } from './autocomplete.module';
 
 export default {
-  title: "atoms/forms/autocomplete",
-  component: AutocompleteComponent,
+    title: 'atoms/forms/autocomplete',
+    component: AutocompleteComponent,
 } as Meta<AutocompleteComponent<string>>;
 
-const template: Story<AutocompleteComponent<string>> = (
-  args: AutocompleteComponent<string>
-) => ({
-  moduleMetadata: {
-    imports: [AutocompleteModule],
-  },
-  props: {
-    ...args,
-    valueChange: action("valueChange"),
-  },
-  template: `
+const template: Story<AutocompleteComponent<string>> = (args: AutocompleteComponent<string>) => ({
+    moduleMetadata: {
+        imports: [AutocompleteModule],
+    },
+    props: {
+        ...args,
+        valueChange: action('valueChange'),
+    },
+    template: `
   <adr-autocomplete [value]="value"
   [openOn]="openOn"
   [options]="options" 
@@ -31,29 +29,28 @@ const template: Story<AutocompleteComponent<string>> = (
 
 export const basic = template.bind({});
 basic.args = {
-  options: ["Gaetan", "Soren", "Bernard"],
-  value: "",
+    options: ['Gaetan', 'Soren', 'Bernard'],
+    value: '',
 };
 
 interface Person {
-  id: number | null;
-  firstName: string;
-  name: string | null;
+    id: number | null;
+    firstName: string;
+    name: string | null;
 }
 
 const objectsValuesTemplate: Story<AutocompleteComponent<Person>> = (
-  args: AutocompleteComponent<Person>
+    args: AutocompleteComponent<Person>,
 ) => ({
-  moduleMetadata: {
-    imports: [AutocompleteModule],
-  },
-  props: {
-    ...args,
-    valueChange: action("valueChange"),
-    displayOptionFn: (option: Person): string =>
-      `${option.firstName} - ${option.name}`,
-  },
-  template: `
+    moduleMetadata: {
+        imports: [AutocompleteModule],
+    },
+    props: {
+        ...args,
+        valueChange: action('valueChange'),
+        displayOptionFn: (option: Person): string => `${option.firstName} - ${option.name}`,
+    },
+    template: `
   <adr-autocomplete [value]="value"
   [openOn]="openOn"
   [options]="options" 
@@ -66,30 +63,29 @@ const objectsValuesTemplate: Story<AutocompleteComponent<Person>> = (
 
 export const complexValues = objectsValuesTemplate.bind({});
 complexValues.args = {
-  options: [
-    { id: 1, firstName: "Gaetan", name: "Redin" },
-    { id: 2, firstName: "Soren", name: "Redin" },
-    { id: 3, firstName: "Lord", name: "Voldemor" },
-  ],
-  value: { id: 2, firstName: "Soren", name: "Redin" },
+    options: [
+        { id: 1, firstName: 'Gaetan', name: 'Redin' },
+        { id: 2, firstName: 'Soren', name: 'Redin' },
+        { id: 3, firstName: 'Lord', name: 'Voldemor' },
+    ],
+    value: { id: 2, firstName: 'Soren', name: 'Redin' },
 };
 
 const reactiveFormTemplate: Story<AutocompleteComponent<Person>> = (
-  args: AutocompleteComponent<Person>
+    args: AutocompleteComponent<Person>,
 ) => ({
-  moduleMetadata: {
-    imports: [AutocompleteModule, ReactiveFormsModule],
-  },
-  props: {
-    ...args,
-    valueChange: action("valueChange"),
-    value: null,
-    displayOptionFn: (option: Person): string =>
-      `${option.firstName} - ${option.name}`,
-    control: new FormControl({ id: 2, firstName: "Soren", name: "Redin" }),
-    identityFn: (p: Person): any => p.id,
-  },
-  template: `
+    moduleMetadata: {
+        imports: [AutocompleteModule, ReactiveFormsModule],
+    },
+    props: {
+        ...args,
+        valueChange: action('valueChange'),
+        value: null,
+        displayOptionFn: (option: Person): string => `${option.firstName} - ${option.name}`,
+        control: new FormControl({ id: 2, firstName: 'Soren', name: 'Redin' }),
+        identityFn: (p: Person): any => p.id,
+    },
+    template: `
   <adr-autocomplete 
   [formControl]="control"
   [openOn]="openOn"
@@ -103,29 +99,28 @@ const reactiveFormTemplate: Story<AutocompleteComponent<Person>> = (
 
 export const reactiveForm = reactiveFormTemplate.bind({});
 reactiveForm.args = {
-  options: [
-    { id: 1, firstName: "Gaetan", name: "Redin" },
-    { id: 2, firstName: "Soren", name: "Redin" },
-    { id: 3, firstName: "Lord", name: "Voldemor" },
-  ],
+    options: [
+        { id: 1, firstName: 'Gaetan', name: 'Redin' },
+        { id: 2, firstName: 'Soren', name: 'Redin' },
+        { id: 3, firstName: 'Lord', name: 'Voldemor' },
+    ],
 };
 
 const addOptionTemplate: Story<AutocompleteComponent<Person>> = (
-  args: AutocompleteComponent<Person>
+    args: AutocompleteComponent<Person>,
 ) => ({
-  moduleMetadata: {
-    imports: [AutocompleteModule, ReactiveFormsModule],
-  },
-  props: {
-    ...args,
-    valueChange: action("valueChange"),
-    value: null,
-    displayOptionFn: (option: Person): string =>
-      `${option.firstName} - ${option.name}`,
-    control: new FormControl({ id: 2, firstName: "Soren", name: "Redin" }),
-    identityFn: (p: Person): any => p.id,
-  },
-  template: `
+    moduleMetadata: {
+        imports: [AutocompleteModule, ReactiveFormsModule],
+    },
+    props: {
+        ...args,
+        valueChange: action('valueChange'),
+        value: null,
+        displayOptionFn: (option: Person): string => `${option.firstName} - ${option.name}`,
+        control: new FormControl({ id: 2, firstName: 'Soren', name: 'Redin' }),
+        identityFn: (p: Person): any => p.id,
+    },
+    template: `
 <adr-autocomplete 
 [formControl]="control" [openOn]="openOn" [options]="options" 
 [required]="required" (valueChange)="valueChange($event)"
@@ -135,23 +130,23 @@ const addOptionTemplate: Story<AutocompleteComponent<Person>> = (
 
 export const addOption = addOptionTemplate.bind({});
 addOption.args = {
-  options: [
-    { id: 1, firstName: "Gaetan", name: "Redin" },
-    { id: 2, firstName: "Soren", name: "Redin" },
-    { id: 3, firstName: "Lord", name: "Voldemor" },
-  ],
-  createOptionFn: function (input: string) {
-    if (input.includes(`-`)) {
-      return {
-        id: null,
-        firstName: input.split(`-`)[0].trim(),
-        name: input.split(`-`)[1].trim(),
-      } as Person;
-    }
-    return {
-      id: null,
-      firstName: input,
-      name: null,
-    } as Person;
-  },
+    options: [
+        { id: 1, firstName: 'Gaetan', name: 'Redin' },
+        { id: 2, firstName: 'Soren', name: 'Redin' },
+        { id: 3, firstName: 'Lord', name: 'Voldemor' },
+    ],
+    createOptionFn: function (input: string) {
+        if (input.includes('-')) {
+            return {
+                id: null,
+                firstName: input.split('-')[0].trim(),
+                name: input.split('-')[1].trim(),
+            } as Person;
+        }
+        return {
+            id: null,
+            firstName: input,
+            name: null,
+        } as Person;
+    },
 };
