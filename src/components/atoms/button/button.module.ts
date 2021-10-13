2e301c224ed3507d8beefecdca2d528d1398ccdc
Gaetan Redin <gaetan.redin@cbp-group.com>
2021-10-13 20:17:44 +0200

    feature(button): inline module
    Closes #51

--- src/components/atoms/button/button.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ButtonDirective } from 'src/components/atoms/button/button.directive';
-
-@NgModule({
-  declarations: [ButtonDirective],
-  exports: [ButtonDirective]
-})
-export class ButtonModule {
-}
